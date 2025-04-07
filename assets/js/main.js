44836eb82f02eb62d350be011003916da2122f17
Midamisha <mitrofanovm226@gmail.com>
2025-04-07 15:19:10 +0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,45 @@
+
+window.onclick = function (event) {
+    if (!event.target.matches('.dropbtn')) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+var dropbtns = document.getElementsByClassName("dropbtn");
+var i;
+for (i = 0; i < dropbtns.length; i++) {
+    dropbtns[i].addEventListener("click", function() {
+        document.getElementById("myDropdown").classList.toggle("show");
+        var panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+        panel.style.display = "none";
+    } else {
+        panel.style.display = "block";
+    }
+    }
+    )
+}
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    }
+    )
+}
